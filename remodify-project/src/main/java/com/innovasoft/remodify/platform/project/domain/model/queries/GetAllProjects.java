7fc540163a25b9944e0fb1a1ffd3d799fea359ca
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-10-18 17:43:36 -0500

    feat(microservices): Added microservice project.

---
+++ remodify-project/src/main/java/com/innovasoft/remodify/platform/project/domain/model/queries/GetAllProjects.java
@@ -0,0 +1,4 @@
+package com.innovasoft.remodify.platform.project.domain.model.queries;
+
+public record GetAllProjects() {
+}
---
+++ remodify-project/src/main/java/com/innovasoft/remodify/platform/project/infrastructure/persistance/jpa/ProjectRepository.java
@@ -0,0 +1,18 @@
+package com.innovasoft.remodify.platform.project.infrastructure.persistance.jpa;
+
+import com.innovasoft.remodify.platform.project.domain.model.aggregates.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findAllByBusinessId(Long businessId);
+
+    boolean existsByName(String name);
+
+    Optional<Project> findById(Long id);
+}
