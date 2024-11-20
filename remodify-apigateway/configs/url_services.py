a70d22e42fe14d3c4b70af6061a91096f3aaafe3
JairC365 <102072560+JairCoraje@users.noreply.github.com>
2024-11-20 04:41:22 -0500

    feat: add Procfile and requirements.txt

--- remodify-apigateway/configs/url_services.py
+++ remodify-apigateway/configs/url_services.py
@@ -1,8 +1,8 @@
 
 # Microservicios URLs
-MICROSERVICE_BUSINESS = "http://localhost:8081/api/v1/businesses"
-MICROSERVICE_IAM = "http://localhost:8082/api/v1"
-MICROSERVICE_PROJECT = "http://localhost:8083/api/v1/projects"
-MICROSERVICE_REVIEWS = "http://localhost:8084/api/v1/reviews"
-MICROSERVICE_PROJECTREQUEST = "http://localhost:8085/api/v1/project-requests"
+MICROSERVICE_BUSINESS = "https://zealous-joy-production.up.railway.app/api/v1/businesses"
+MICROSERVICE_IAM = "https://renewed-radiance-production.up.railway.app/api/v1"
+MICROSERVICE_PROJECT = "https://heartfelt-insight-production.up.railway.app/api/v1/projects"
+MICROSERVICE_REVIEWS = "https://caring-joy-production.up.railway.app/api/v1/reviews"
+MICROSERVICE_PROJECTREQUEST = "https://inspiring-consideration-production.up.railway.app/api/v1/project-requests"
 
