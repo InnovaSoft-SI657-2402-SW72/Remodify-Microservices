8f1c7d27a876d090b6bf4acab41d0f3e9beffbdd
JairC365 <102072560+JairCoraje@users.noreply.github.com>
2024-11-21 03:02:59 -0500

    feat: add dockerfile and update url_services.py

--- remodify-apigateway/configs/url_services.py
+++ remodify-apigateway/configs/url_services.py
@@ -1,16 +1,7 @@
+MICROSERVICE_BUSINESS = "http://13.91.167.59:8081/api/v1/businesses"
+MICROSERVICE_IAM = "http://13.91.167.59:8082/api/v1"
+MICROSERVICE_PROJECT = "http://13.91.167.59:8083/api/v1/projects"
+MICROSERVICE_REVIEWS = "http://13.91.167.59:8084/api/v1/reviews"
+MICROSERVICE_PROJECT_REQUEST = "http://13.91.167.59:8085/api/v1/project-requests"
 
-# Microservicios URLs
-
-MICROSERVICE_BUSINESS = "https://zealous-joy-production.up.railway.app/api/v1/businesses"
-MICROSERVICE_IAM = "https://renewed-radiance-production.up.railway.app/api/v1"
-MICROSERVICE_PROJECT = "https://heartfelt-insight-production.up.railway.app/api/v1/projects"
-MICROSERVICE_REVIEWS = "https://caring-joy-production.up.railway.app/api/v1/reviews"
-MICROSERVICE_PROJECTREQUEST = "https://inspiring-consideration-production.up.railway.app/api/v1/project-requests"
-
-#MICROSERVICE_BUSINESS = "http://localhost:8081/api/v1/businesses"
-#MICROSERVICE_IAM = "http://localhost:8082/api/v1"
-#MICROSERVICE_PROJECT = "http://localhost:8083/api/v1/projects"
-#MICROSERVICE_REVIEWS = "http://localhost:8084/api/v1/reviews"
-#MICROSERVICE_PROJECT_REQUEST = "http://localhost:8085/api/v1/project-requests"
-#
 
